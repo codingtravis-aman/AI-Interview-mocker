88fa62fb152afadd1b4af2deafa2692c8dc9835f
Aman Jha <61490300+codingtravis-aman@users.noreply.github.com>
2024-10-11 08:57:46 +0530

    Update planData.jsx

--- utils/planData.jsx
+++ utils/planData.jsx
@@ -5,7 +5,7 @@ export default[
         cost:0,
         offering:[
             {
-                value:'✔️ Create 3 Free Mock Interview'
+                value:'✔️ Create 4 Free Mock Interview'
             },
             {
                 value:'✔️ Unlimited Retake Interview'
